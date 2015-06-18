0c6da113444b9284813fca6836881ffce1962289
David Fudge <rkstar@mac.com>
2015-06-18 01:10:00 -0400

    added dom tree traversal
    code will now traverse entire dom tree down the line to polymerize all deep-nested nodes
    
    Signed-off-by: David Fudge <rkstar@mac.com>

--- client/lib/mixins/polymer.js
+++ client/lib/mixins/polymer.js
@@ -1,28 +1,32 @@
 Polymer = {
   Mixin: {
     _polymer_attribute: 'data-polymer',
-    componentDidMount: function(){
-      var nodelist = this.getDOMNode().querySelectorAll("["+this._polymer_attribute+"]"),
-        nodes = Array.prototype.slice.call(nodelist),
-        polymer_attrs,
-        attreq
-      nodes.unshift(this.getDOMNode())
-      nodes.map(function(el){
-        polymer_attrs = el.getAttribute(this._polymer_attribute)
-        if( !polymer_attrs ){
-          return
-        }
+
+    _polymerize: function( node ){
+      var polymer_attrs = (node.hasAttribute && node.hasAttribute(this._polymer_attribute)) ? node.getAttribute(this._polymer_attribute) : null,
+        childNodes = Array.prototype.slice.call(node.childNodes)
+
+      if( polymer_attrs ){
+        var attr, attreq
         polymer_attrs.split(',').map(function(attr){
           attr = attr.trim()
           if( attr.indexOf("=") > -1 ){
             attreq = attr.split('=')
-            el.setAttribute(attreq[0],attreq[1])
+            node.setAttribute(attreq[0],attreq[1])
           } else {
-            el.setAttribute(attr,'')
+            node.setAttribute(attr,'')
           }
         })
-        el.removeAttribute(this._polymer_attribute)
+        node.removeAttribute(this._polymer_attribute)
+      }
+
+      childNodes.map(function(child){
+        this._polymerize(child)
       }, this)
+    },
+
+    componentDidMount: function(){
+      this._polymerize(this.getDOMNode())
     }
   }
 }
